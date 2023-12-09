97e80f8ac2541a393a9478257f4e1d228d13d60f
RIFKI ARDIANSAH <77479147+siripki@users.noreply.github.com>
2023-12-09 21:49:31 +0700

    Delete normalMode.ino

--- normalMode.ino
+++
@@ -1,4 +0,0 @@
-void normalMode() {
-  sensorMeasurement();
-  sendData("{\"" + tempKey + "\":" + String(temp, 1) + ",\"" + phKey + "\":" + String(phValue, 1) + ",\"" + wlKey + "\":" + String(wlValue, 1) + "}");
-}
